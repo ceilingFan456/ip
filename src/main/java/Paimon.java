a9c609e96ae6d1888951147d285882bf58c0a6f5
Qiming <e0271228@u.nus.edu>
2025-01-20 15:26:05 +0800

    Add ability to repeat words.

--- src/main/java/Paimon.java
+++ src/main/java/Paimon.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class Paimon {
+    private Scanner sc;
 
     // constructor
     public Paimon() {
+        this.sc = new Scanner(System.in);
     }
 
     public void run() {
+        // greeting message
         System.out.println("Hello! I'm Paimon");
         System.out.println("What can I do for you?\n");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        // loop variables
+        boolean running = true;
+
+        // main loop
+        while (running) {
+            // read a str from user
+            String str = sc.nextLine();
+            
+            switch (str) {
+                case "bye":
+                    running = false;
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+            
+                default:
+                    System.out.println(str + "\n");
+                    break;
+            }
+        }
     }
     public static void main(String[] args) {
         new Paimon().run();
