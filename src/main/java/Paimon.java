da9d51cbae6ec35cdba9a4dbe75c18d02ceab965
Qiming <e0271228@u.nus.edu>
2025-01-20 15:03:34 +0800

    Rename Duke to Paimon

---
+++ src/main/java/Paimon.java
@@ -0,0 +1,16 @@
+public class Paimon {
+
+    // constructor
+    public Paimon() {
+    }
+
+    public void run() {
+        System.out.println("Hello! I'm Paimon");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void main(String[] args) {
+        new Paimon().run();
+    }
+}
